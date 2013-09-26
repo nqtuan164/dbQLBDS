18c71582bd99877d777f2ef43c1ceb2bbccc6af4
Nguyễn Quốc Tuấn <nqtuan164@gmail.com>
2013-09-27 00:57:08 +0700

    COMMIT Project

---
+++ SOURCE/QLBDS/QLBDS/Controllers/Admin/TaiKhoanController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLBDS.Controllers.Admin
+{
+    public class TaiKhoanController : Controller
+    {
+        //
+        // GET: /TaiKhoan_/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

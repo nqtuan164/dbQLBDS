6afa49e7dae9341f591270864308a9cc0824cb6b
Nguyễn Quốc Tuấn <nqtuan164@gmail.com>
2013-09-27 14:36:32 +0700

    Commit new project

---
+++ SOURCE/QLBDS/Controllers/Admin/CanHoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLBDS.Controllers.Admin
+{
+    public class CanHoController : Controller
+    {
+        //
+        // GET: /CanHo/
+
+        public ActionResult Index()
+        {
+            return View("~/Views/Admin/CanHo/Index.cshtml");
+        }
+
+        //
+        // POST: /CanHo/TaoCanHo
+        public ActionResult TaoCanHo()
+        {
+            return View("~/Views/Admin/CanHo/TaoCanHo.cshtml");
+        }
+
+        //
+        // POST: /CanHo/ChinhSuaCanHo
+        public ActionResult ChinhSuaCanHo(int id)
+        {
+            return View("~/Views/Admin/CanHo/TaoCanHo.cshtml");
+        }
+
+    }
+}
---
+++ SOURCE/QLBDS/Controllers/Admin/NhanGiaoDichController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLBDS.Controllers.Admin
+{
+    public class NhanGiaoDichController : Controller
+    {
+        //
+        // GET: /NhanGiaoDich_/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

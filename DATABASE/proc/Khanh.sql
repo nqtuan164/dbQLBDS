99eded2b2afecb7f10eb7565b4d36994d25d2a13
Dương Diệu Pháp <phap@imageglass.org>
2013-09-27 17:59:03 +0700

    v9 - update proc

---
+++ DATABASE/proc/Khanh.sql
@@ -0,0 +1,58 @@
+﻿CREATE PROC sp_dangNhapTaiKhoan
+	@email nvarchar(100), @matkhau nvarchar(100)
+AS
+BEGIN
+	IF(NOT EXISTS(SELECT * FROM taikhoan WHERE email = @email AND matkhau = @matkhau))
+	BEGIN
+		RAISERROR('Đăng nhập không thành công', 16, 1)
+	END
+END
+GO
+--exec sp_dangNhapTaiKhoan 'd2phap@gmail.com', phapabcdf
+
+CREATE PROC sp_dangKyTaiKhoan
+	@email nvarchar(100), @matkhau nvarchar(100), @maloaitaikhoan int, 
+	@ten nvarchar(100), @ngaysinh datetime, @diachi nvarchar(255),	
+	@dienthoai nvarchar(100), @ngaydangky datetime, @trangthai int
+AS
+BEGIN
+	IF(EXISTS(SELECT email FROM taikhoan WHERE email = @email)) BEGIN
+		RAISERROR('Email đã tồn tại',16,1)
+	END
+	ELSE BEGIN
+		INSERT INTO taikhoan(email, matkhau, maloaitaikhoan, ten, ngaysinh, diachi, dienthoai, ngaydangky, trangthai)
+		VALUES(@email, @matkhau, @maloaitaikhoan, @ten, @ngaysinh, @diachi, @dienthoai, @ngaydangky, @trangthai)
+	END
+END
+GO
+
+CREATE PROC sp_capNhatTaiKhoan
+	@email nvarchar(100), @matkhau nvarchar(100), @maloaitaikhoan int, 
+	@ten nvarchar(100), @ngaysinh datetime, @diachi nvarchar(255), 
+	@dienthoai nvarchar(100), @ngaydangky datetime
+AS
+BEGIN
+	UPDATE taikhoan 
+	SET matkhau = @matkhau, maloaitaikhoan = @maloaitaikhoan, ten = @ten,
+		ngaysinh = @ngaysinh, diachi = @diachi, dienthoai = @dienthoai,
+		ngaydangky = @ngaydangky
+	WHERE email = @email
+END
+GO
+
+CREATE PROC sp_capNhatTrangThaiTaiKhoan
+	@email nvarchar(100), @trangthai int
+AS
+BEGIN
+	IF(NOT EXISTS(SELECT * FROM taikhoan WHERE email = @email))
+	BEGIN
+		RAISERROR('Email không tồn tại',16,1)
+	END
+	
+	UPDATE taikhoan 
+	SET trangthai = @trangthai 
+	WHERE email = @email
+END
+GO
+
+--exec sp_capNhatTrangThaiTaiKhoan 'd2phap@gmail.comxxx', 0
---
+++ DATABASE/proc/Loc.sql
@@ -0,0 +1,80 @@
+﻿/*
+*		STORED PROCEDURES
+*		Bùi Bá Lộc - 1241363
+*/
+
+--
+CREATE PROC sp_thuecanho
+	@mataikhoan int, @macanho int, @tiencoc float, @thoigianthue datetime, @thoigiankethuc datetime,
+	@thoigiangiaodich datetime, @dienthoai nvarchar(100), @diachi nvarchar(100), @ghichu text
+AS
+BEGIN
+	IF NOT EXISTS(SELECT * FROM canho WHERE macanho = @macanho)
+		BEGIN
+			RAISERROR(N'Mã căn hộ không tồn tại', 16, 9)
+		END
+	ELSE
+		BEGIN
+			INSERT INTO thuecanho(mataikhoan, macanho, tiencoc, thoigianthue, thoigianketthuc,
+								thoigiangiaodich, dienthoai, diachi, ghichu, kichhoat)
+			VALUES(@mataikhoan, @macanho, @tiencoc, @thoigianthue, @thoigiankethuc, 
+				@thoigiangiaodich, @dienthoai, @diachi, @ghichu, 1)
+		END
+END
+GO
+--
+
+CREATE PROC sp_xoathuecanho
+	@mathuecanho int
+AS
+BEGIN
+	IF NOT EXISTS(SELECT * FROM thuecanho WHERE mathuecanho = @mathuecanho)
+		BEGIN
+			RAISERROR(N'Mã thuê căn hộ không tồn tại', 16, 9)
+		END
+	ELSE
+		BEGIN
+			DELETE FROM nhangiaodich WHERE mathuecanho = @mathuecanho
+			DELETE FROM thuecanho WHERE mathuecanho = @mathuecanho
+		END	
+END
+GO
+--
+
+CREATE PROC sp_suathuecanho
+	@mathuecanho int, @mataikhoan int, @macanho int, @tiencoc float, @thoigianthue datetime, 
+	@thoigiankethuc datetime, @thoigiangiaodich datetime, @dienthoai nvarchar(100), 
+	@diachi nvarchar(100), @ghichu text, @kichhoat int
+AS
+BEGIN
+	IF NOT EXISTS(SELECT * FROM thuecanho WHERE mathuecanho = @mathuecanho)
+		BEGIN
+			RAISERROR(N'Mã thuê căn hộ không tồn tại', 16, 9)
+		END
+	ELSE
+		BEGIN
+			UPDATE thuecanho
+			SET  mataikhoan = @mataikhoan, macanho = @macanho, tiencoc = @tiencoc, thoigianthue = @thoigianthue,
+				thoigianketthuc = @thoigiankethuc, thoigiangiaodich = @thoigiangiaodich, dienthoai = @dienthoai, diachi = @diachi,
+				ghichu = @ghichu, kichhoat = @kichhoat
+			WHERE mathuecanho = @mathuecanho
+		END	
+END
+GO
+--
+
+CREATE PROC sp_xemcanho
+	@macanho int
+AS
+BEGIN
+	IF NOT EXISTS(SELECT * FROM canho WHERE macanho = @macanho)
+		BEGIN
+			RAISERROR(N'Mã thuê căn hộ không tồn tại', 16, 9)
+		END
+	ELSE
+		BEGIN
+			SELECT * FROM canho WHERE macanho = @macanho
+		END	
+END
+GO
+

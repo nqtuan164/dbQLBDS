27b1950fa7786a523ce8c5015fb2f62bb3f4fbb4
Dương Diệu Pháp <phap@imageglass.org>
2013-10-06 23:31:00 +0700

    v17 Tim kiem (design)

---
+++ SOURCE/dbQLBDS/dbQLBDS/Controllers/TimKiemController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbQLBDS.Controllers
+{
+    public class TimKiemController : Controller
+    {
+        //
+        // GET: /TimKiem/
+
+        public ActionResult Index()
+        {
+            return PartialView("~/Views/Shared/TimKiem.cshtml");
+        }
+
+    }
+}

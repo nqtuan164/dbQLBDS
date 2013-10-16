b2c8d283e0c66ebfec872e0f4a306f0e34a3b928
Dương Diệu Pháp <phap@imageglass.org>
2013-10-17 00:07:06 +0700

    v28 Xong giao diện người dùng cuối
    - Trang chủ
    - Tìm kiếm
    - Xem thông tin căn hộ

---
+++ SOURCE/dbQLBDS/dbQLBDS/Controllers/ChiTietCanHoController.cs
@@ -0,0 +1,118 @@
+﻿using dbQLBDS.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbQLBDS.Controllers
+{
+    public class ChiTietCanHoController : Controller
+    {
+        //
+        // GET: /CanHo/
+
+        public ActionResult Index()
+        {
+            int canHoID = 0;
+            CanHo ch = new CanHo();
+
+            if (!Int32.TryParse(Request.Params["id"], out canHoID))
+            {
+                ch.MaTrangThaiCanHo = -999;
+            }
+            else
+            {
+                DataProvider dp = new DataProvider();
+
+                //Load danh sach thanh pho
+                string sql = @"SELECT ch.*, d.tenduong, q.tenquan, tp.tenthanhpho
+                                FROM canho ch, duong d, quan q, thanhpho tp
+                                WHERE ch.kichhoat = 1 AND
+                                    ch.matrangthaicanho = 2 AND
+                                    ch.maduong = d.maduong AND
+                                    d.maquan = q.maquan AND
+                                    q.mathanhpho = tp.mathanhpho AND
+                                    ch.macanho = " + canHoID.ToString() + @"
+                                ORDER BY ch.ngaydang DESC
+                                ";
+                DataTable dt = new DataTable();
+                dt = dp.ExecuteQuery(sql);
+
+                if (dt.Rows.Count > 0)
+                {
+                    ch.MaCanHo = (int)dt.Rows[0]["macanho"];
+                    ch.TenCanHo = dt.Rows[0]["tencanho"].ToString();
+                    ch.MaDuong = (int)dt.Rows[0]["maduong"];
+                    ch.DiaChi = dt.Rows[0]["diachi"].ToString() + " " +
+                                    dt.Rows[0]["tenduong"].ToString() + ", " +
+                                    dt.Rows[0]["tenquan"].ToString() + ", " +
+                                    dt.Rows[0]["tenthanhpho"].ToString();
+                    ch.MieuTa = dt.Rows[0]["mieuta"].ToString();
+                    ch.ToaDo = dt.Rows[0]["toado"].ToString();
+                    ch.GiaThue = (double)dt.Rows[0]["giathue"];
+                    ch.DienTich = (double)dt.Rows[0]["dientich"];
+                    ch.MaTrangThaiCanHo = (int)dt.Rows[0]["matrangthaicanho"];
+                    ch.TrangThaiCanHo = (TrangThaiCanHo)dt.Rows[0]["matrangthaicanho"];
+                    ch.NgayDang = DateTime.Parse(dt.Rows[0]["ngaydang"].ToString());
+                    ch.NguoiDang = (int)dt.Rows[0]["nguoidang"];
+                    ch.GhiChu = dt.Rows[0]["ghichu"].ToString();
+                    ch.KichHoat = (int)dt.Rows[0]["kichhoat"];
+
+
+                    //Lay hinh anh
+                    sql = @"SELECT *
+                            FROM hinhanhcanho
+                            WHERE macanho = " + canHoID.ToString();
+                    dt = new DataTable();
+                    dt = dp.ExecuteQuery(sql);
+                    List<HinhAnhCanHo> dsHinhAnhCanHo = new List<HinhAnhCanHo>();
+
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        HinhAnhCanHo h = new HinhAnhCanHo();
+                        h.MaHinhAnh = (int)dt.Rows[i]["mahinhanh"];
+                        h.LienKet = dt.Rows[i]["lienket"].ToString();
+                        h.MaCanHo = canHoID;
+
+                        dsHinhAnhCanHo.Add(h);
+                    }
+                    ViewBag.dsHinhAnhCanHo = dsHinhAnhCanHo;
+
+
+                    //Lay thong tin nguoi dang tin
+                    sql = @"SELECT *
+                            FROM taikhoan
+                            WHERE mataikhoan = " + ch.NguoiDang.ToString();
+                    dt = new DataTable();
+                    dt = dp.ExecuteQuery(sql);
+
+                    TaiKhoan tk = new TaiKhoan();
+                    tk.MaTaiKhoan = (int)dt.Rows[0]["mataikhoan"];
+                    tk.Email = dt.Rows[0]["email"].ToString();
+                    tk.MaLoaiTaiKhoan = (int)dt.Rows[0]["maloaitaikhoan"];
+                    tk.LoaiTaiKhoan = (LoaiTaiKhoan)dt.Rows[0]["maloaitaikhoan"];
+                    tk.Ten = dt.Rows[0]["ten"].ToString();
+                    tk.NgaySinh = (DateTime)dt.Rows[0]["ngaysinh"];
+                    tk.DiaChi = dt.Rows[0]["diachi"].ToString();
+                    tk.DienThoai = dt.Rows[0]["dienthoai"].ToString();
+                    tk.NgayDangKy = (DateTime)dt.Rows[0]["ngaydangky"];
+                    tk.MaTrangThai = (int)dt.Rows[0]["trangthai"];
+                    tk.TrangThai = (TrangThaiTaiKhoan)dt.Rows[0]["trangthai"];
+
+                    ViewBag.taiKhoan = tk;
+                }
+                else
+                {
+                    ch.MaTrangThaiCanHo = -999;
+                }
+            }
+
+
+            return View("~/Views/Shared/ChiTietCanHo.cshtml", ch);
+        }
+
+
+    }
+}
--- SOURCE/dbQLBDS/dbQLBDS/Controllers/IndexController.cs
+++ SOURCE/dbQLBDS/dbQLBDS/Controllers/IndexController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using dbQLBDS.Controllers;
+using dbQLBDS.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,8 +16,50 @@ namespace QLBDS.Controllers
 
         public ActionResult Index()
         {
-            return View("~/Views/Index.cshtml");
-            //return Request.Url.Host ;
+            //Load danh sach thanh pho
+            DataProvider dp = new DataProvider();
+            string sql = @"SELECT TOP 8 ch.*, d.tenduong, q.tenquan, tp.tenthanhpho
+                        FROM canho ch, duong d, quan q, thanhpho tp
+                        WHERE ch.kichhoat = 1 AND
+	                        ch.matrangthaicanho = 2 AND
+	                        ch.maduong = d.maduong AND
+	                        d.maquan = q.maquan AND
+	                        q.mathanhpho = tp.mathanhpho
+                        ORDER BY ch.ngaydang DESC
+                        ";
+            DataTable dt = new DataTable();
+            dt = dp.ExecuteQuery(sql);
+
+            List<CanHo> dsCanHo = new List<CanHo>();
+            if (dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    CanHo item = new CanHo();
+                    item.MaCanHo = (int)dt.Rows[i]["macanho"];
+                    item.TenCanHo = dt.Rows[i]["tencanho"].ToString();
+                    item.MaDuong = (int)dt.Rows[i]["maduong"];
+                    item.DiaChi = dt.Rows[i]["diachi"].ToString() + " " +
+                                    dt.Rows[i]["tenduong"].ToString() + ", " +
+                                    dt.Rows[i]["tenquan"].ToString() + ", " +
+                                    dt.Rows[i]["tenthanhpho"].ToString();
+                    item.MieuTa = dt.Rows[i]["mieuta"].ToString();
+                    item.ToaDo = dt.Rows[i]["toado"].ToString();
+                    item.GiaThue = (double)dt.Rows[i]["giathue"];
+                    item.DienTich = (double)dt.Rows[i]["dientich"];
+                    item.MaTrangThaiCanHo = (int)dt.Rows[i]["matrangthaicanho"];
+                    item.TrangThaiCanHo = (TrangThaiCanHo)dt.Rows[i]["matrangthaicanho"];
+                    item.NgayDang = DateTime.Parse(dt.Rows[i]["ngaydang"].ToString());
+                    item.NguoiDang = (int)dt.Rows[i]["nguoidang"];
+                    item.GhiChu = dt.Rows[i]["ghichu"].ToString();
+                    item.KichHoat = (int)dt.Rows[i]["kichhoat"];
+
+                    dsCanHo.Add(item);
+                }
+            }
+
+            return View("~/Views/Index.cshtml", dsCanHo);
+            //return Request.Url.Host;
         }
 
     }
